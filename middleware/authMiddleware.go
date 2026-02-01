a464c353e1f04f78192a9eeef13442c9121c654d
Ansar <kelesansar62@gmail.com>
2026-02-01 23:09:27 +0500

    added middleware go file

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,51 @@
+package middleware
+
+import (
+	"car-management-system/config"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(401, gin.H{"error": "unauthorized"})
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.Config.JwtSecretKey), nil
+		})
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(401, gin.H{"error": "invalid token"})
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.AbortWithStatusJSON(401, gin.H{"error": "invalid claims"})
+			return
+		}
+
+		sub, ok := claims["sub"].(string)
+		if !ok {
+			c.AbortWithStatusJSON(401, gin.H{"error": "invalid subject"})
+			return
+		}
+
+		userId, err := strconv.Atoi(sub)
+		if err != nil {
+			c.AbortWithStatusJSON(401, gin.H{"error": "invalid user id"})
+			return
+		}
+
+		c.Set("user_id", userId)
+		c.Next()
+	}
+}
