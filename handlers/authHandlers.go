c843bc00eb9284260c80b326859ebf03c75497f9
Ansar <kelesansar62@gmail.com>
2026-02-01 23:10:06 +0500

    handlers of users

---
+++ handlers/authHandlers.go
@@ -0,0 +1,96 @@
+package handlers
+
+import (
+	"car-management-system/config"
+	"car-management-system/models"
+	"car-management-system/repositories"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthHandler struct {
+	authRepo *repositories.AuthRepository
+}
+
+func NewAuthHandler(authRepo *repositories.AuthRepository) *AuthHandler {
+	return &AuthHandler{authRepo: authRepo}
+}
+
+func (handler *AuthHandler) SignUp(c *gin.Context) {
+	var request models.RegisterUserRequest
+
+	err := c.BindJSON(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("could not bind json body"))
+		return
+	}
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(request.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("could not hash password"))
+		return
+	}
+	user := models.User{
+		Full_name:    request.Full_name,
+		Email:        request.Email,
+		PasswordHash: string(passwordHash),
+	}
+
+	id, err := handler.authRepo.Create(c, user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("could not create an user"))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": id})
+}
+
+func (handler *AuthHandler) GetAll(c *gin.Context) {
+	users, err := handler.authRepo.FindAll(c)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+	}
+	c.JSON(http.StatusOK, users)
+}
+
+func (handler *AuthHandler) SignIn(c *gin.Context) {
+	var request models.SignInRequest
+	err := c.BindJSON(&request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("could bind json body"))
+		return
+	}
+
+	user, err := handler.authRepo.FindEmail(c, request.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.NewApiError("could not find the email"))
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(request.Password))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, models.NewApiError("could not sign in"))
+		return
+	}
+	claims := jwt.RegisteredClaims{
+		Subject:   strconv.Itoa(user.User_id),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(config.Config.JwtExpiresIn)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(config.Config.JwtSecretKey))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.NewApiError("could not sign JWT"))
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
+
+func (handler *AuthHandler) SignOut(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "successfully exited",
+	})
+}
